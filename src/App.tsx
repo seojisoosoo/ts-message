eaf9cf88e9b17f46d768a60b61e19345ca08058a
Seojisoo20191941 <amy0480@naver.com>
2022-12-28 04:53:48 +0900

    style : 글로벌 스타일 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Router from './Router';
 import styled from 'styled-components';
+import GlobalStyle from './styles/globalStyle';
 
 export default function App() {
   return (
     <StAppWrapper>
+      <GlobalStyle/>
         <Router/>
     </StAppWrapper>
   )
@@ -14,4 +16,6 @@ const StAppWrapper=styled.section`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  font-family: KOTRAHOPE;
 `
